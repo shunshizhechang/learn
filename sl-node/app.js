8b4302cad79a997b9e4e16662984b1987ad5b5ed
shunshizhechang <495047995@qq.com>
2016-08-19 10:42:24 +0800

    bbs网页

---
+++ sl-node/app.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+//var routes = require('./routes/index');
+//var users = require('./routes/users');
+var senddata = require('./routes/senddata');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.all('*', function (req, res, next) {//解决跨域问题
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
+//app.use('/', routes);
+//app.use('/users', users);
+app.get('/getData', senddata.select);
+app.get('/bbsdata', senddata.bbsdata);
+app.post('/bbsdata', senddata.postbbsdata);
+app.post('/getData', senddata.post);
+app.post('/User/Register', senddata.register);
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
---
+++ sl-node/routes/senddata.js
@@ -0,0 +1,207 @@
+﻿/**
+ * getdata/get
+ * @param request
+ * @param response
+ */
+function select(request, response) {
+    var pg = require('pg'); //链接postgresql数据库
+    var conString = "tcp://postgres:0017@localhost/postgres";
+    var client = new pg.Client(conString);  //构造一个数据库对象  
+    client.connect(function (error, results) {
+        if (error) {
+            console.log('ClientConnectionReady Error: ' + error.message);
+            client.end();
+            return;
+        }
+
+
+        client.query("select * from company;", function (error, results) {
+            console.log("in callback function.\n");
+            if (error) {
+                console.log("error");
+                console.log('GetData Error: ' + error.message);
+                client.end();
+                return;
+            }
+            if (results.rowCount > 0) {
+                //callback(results);   
+                //指定为json格式输出   
+                response.writeHead(200, {"Content-Type": "application/json;charset=utf-8"});
+
+                //先将results 字符串内容转化成json格式，然后响应到浏览器上   
+
+                response.write(JSON.stringify(results.rows));
+                response.end();
+            }
+        })
+
+    });
+}
+/**
+ * getdata/post
+ * @param request
+ * @param response
+ */
+function post(request, response) {
+    var pg = require('pg'); //链接postgresql数据库
+    var conString = "tcp://postgres:0017@localhost/postgres";
+    var client = new pg.Client(conString);  //构造一个数据库对象  
+    client.connect(function (error, results) {
+        if (error) {
+            console.log('ClientConnectionReady Error: ' + error.message);
+            client.end();
+            return;
+        }
+
+
+        client.query("select * from ur;", function (error, results) {
+            console.log("in callback function.\n");
+            if (error) {
+                console.log("error");
+                console.log('GetData Error: ' + error.message);
+                client.end();
+                return;
+            }
+            if (results.rowCount > 0) {
+                for (var i = 0; i < results.rowCount; i++) {
+                    if (request.query.username == results.rows[i].name && request.query.password == results.rows[i].password) {
+                        response.writeHead(200, {"Content-Type": "application/json;charset=utf-8"});
+
+                        //先将results 字符串内容转化成json格式，然后响应到浏览器上
+
+                        response.write(JSON.stringify(request.query.username));
+                        response.end();
+                    }
+                }
+                ;
+
+            }
+        })
+
+    });
+}
+/**
+ * register/post
+ * @param request
+ * @param response
+ */
+function register(request, response) {
+    var pg = require('pg'); //链接postgresql数据库
+    var conString = "tcp://postgres:0017@localhost/postgres";
+    var client = new pg.Client(conString);  //构造一个数据库对象
+	var insert;
+    client.connect(function (error, results) {
+        if (error) {
+            console.log('ClientConnectionReady Error: ' + error.message);
+            client.end();
+            return;
+        }
+        insert = "insert into ur values(" + "'" + request.body.Username + "','" + request.body.Password + "');";
+		 console.log(insert);
+        client.query(insert, function (error, results) {
+            console.log("in callback function.\n");
+            if (error) {
+                console.log("error");
+                console.log('GetData Error: ' + error.message);
+                client.end();
+                return;
+            }
+
+            response.writeHead(200, {"Content-Type": "application/json;charset=utf-8"});
+
+            //先将results 字符串内容转化成json格式，然后响应到浏览器上
+
+            response.write(JSON.stringify((results.rowCount) + 1));
+            response.end();
+
+        })
+
+    });
+}
+
+function bbsdata(request, response) {
+    var pg = require('pg'); //链接postgresql数据库
+    var conString = "tcp://postgres:0017@localhost/sl";//数据库为sl
+    var client = new pg.Client(conString);  //构造一个数据库对象  
+    client.connect(function (error, results) {
+        if (error) {
+            console.log('ClientConnectionReady Error: ' + error.message);
+            client.end();
+            return;
+        }
+
+
+        client.query("select * from bbs;", function (error, results) {
+            console.log("in callback function.\n");
+            if (error) {
+                console.log("error");
+                console.log('GetData Error: ' + error.message);
+                client.end();
+                return;
+            }
+            if (results.rowCount > 0) {
+                //callback(results);   
+                //指定为json格式输出   
+                response.writeHead(200, {"Content-Type": "application/json;charset=utf-8"});
+
+                //先将results 字符串内容转化成json格式，然后响应到浏览器上   
+
+                response.write(JSON.stringify(results.rows));
+                response.end();
+            }
+        })
+
+    });
+}
+
+function postbbsdata(request, response) {
+    var pg = require('pg'); //链接postgresql数据库
+    var conString = "tcp://postgres:0017@localhost/sl";//数据库为sl
+    var client = new pg.Client(conString);  //构造一个数据库对象  
+    client.connect(function (error, results) {
+        if (error) {
+            console.log('ClientConnectionReady Error: ' + error.message);
+            client.end();
+            return;
+        }
+
+	var insert = "INSERT INTO bbs VALUES ('" + request.query.context + "','" + request.query.topic + "','" + request.query.nameg +  "','2016-8-8');";
+	//var insert="INSERT INTO bbs VALUES ('Cheese','hdfjhsd','hfhsdfh','2010-12-10');"
+        client.query(insert, function (error, results) {
+            console.log("in callback function.\n");
+            if (error) {
+                console.log("error");
+                console.log('GetData Error: ' + error.message);
+                client.end();
+                return;
+            }
+            
+        })
+		client.query("select * from bbs;", function (error, results) {
+            console.log("in callback function.\n");
+            if (error) {
+                console.log("error");
+                console.log('GetData Error: ' + error.message);
+                client.end();
+                return;
+            }
+            if (results.rowCount > 0) {
+                //callback(results);   
+                //指定为json格式输出   
+                response.writeHead(200, {"Content-Type": "application/json;charset=utf-8"});
+
+                //先将results 字符串内容转化成json格式，然后响应到浏览器上   
+
+                response.write(JSON.stringify(results.rows));
+                response.end();
+            }
+        })
+
+    });
+}
+
+exports.post = post;
+exports.register = register;
+exports.select = select;  
+exports.bbsdata = bbsdata; 
+exports.postbbsdata = postbbsdata; 
